a8b17bf59bc79720d363815a41a66200e4f7110f
Nagy Márk <marci.2003.11@gmail.com>
2026-08-17 15:16:32 +0200

    Implemented logout function

--- app/src/main/java/com/nagy_mark/mygamevault/Fragments/SettingsFragment.java
+++ app/src/main/java/com/nagy_mark/mygamevault/Fragments/SettingsFragment.java
@@ -1,60 +1,27 @@
 package com.nagy_mark.mygamevault.Fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.nagy_mark.mygamevault.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SettingsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +29,31 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(v -> {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setIcon(R.drawable.ic_warning)
+                    .setTitle(getString(R.string.logout))
+                    .setMessage(getString(R.string.logout_message))
+                    .setPositiveButton(getString(R.string.yes), (dialog, which) -> {
+                        SharedPreferences prefs = requireActivity().getSharedPreferences("MyGameVaultPrefs", Context.MODE_PRIVATE);
+                        prefs.edit().remove("JWT_TOKEN").apply();
+
+                        Toast.makeText(getContext(), getString(R.string.success_logout), Toast.LENGTH_SHORT).show();
+
+                        Navigation.findNavController(v).navigate(R.id.action_settingsFragment_to_loginFragment);
+                    })
+                    .setNegativeButton(getString(R.string.cancel), (dialog, which) -> {
+                        dialog.dismiss();
+                    })
+                    .show();
+        });
     }
 }
