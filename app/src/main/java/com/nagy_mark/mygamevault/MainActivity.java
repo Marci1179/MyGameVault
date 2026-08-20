3cb4949482f8ddbbf412f3093827cd8c6916b94c
Nagy Márk <199969529+Marci1179@users.noreply.github.com>
2026-08-20 13:43:31 +0200

    Added layuot for a search item

--- app/src/main/java/com/nagy_mark/mygamevault/MainActivity.java
+++ app/src/main/java/com/nagy_mark/mygamevault/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clMain), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
