d53e46f466e2016bb91ee79d2f650683b132c336
Nagy Márk <199969529+Marci1179@users.noreply.github.com>
2026-09-11 11:24:31 +0200

    Remove getStoreName method from PriceCheckWorker

--- app/src/main/java/com/nagy_mark/mygamevault/workers/PriceCheckWorker.java
+++ app/src/main/java/com/nagy_mark/mygamevault/workers/PriceCheckWorker.java
@@ -26,6 +26,7 @@ import com.nagy_mark.mygamevault.network.CheapSharkApi;
 import com.nagy_mark.mygamevault.network.CheapSharkApiClient;
 import com.nagy_mark.mygamevault.network.SupabaseApi;
 import com.nagy_mark.mygamevault.network.SupabaseApiClient;
+import com.nagy_mark.mygamevault.utils.CheapSharkUtils;
 import com.nagy_mark.mygamevault.utils.SessionManager;
 
 import java.util.List;
@@ -78,7 +79,7 @@ public class PriceCheckWorker extends Worker {
                         String bestStoreName = "";
 
                         for (CheapSharkDealInfo deal : detailResponse.body().getDeals()) {
-                            String storeName = getStoreName(deal.getStoreId());
+                            String storeName = CheapSharkUtils.getStoreName(deal.getStoreId());
                             if (storeName != null) {
                                 try {
                                     double currentPrice = Double.parseDouble(deal.getPrice());
@@ -151,19 +152,5 @@ public class PriceCheckWorker extends Worker {
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
         notificationManagerCompat.notify(gameTitle.hashCode(), builder.build());
-    }
-
-    private String getStoreName(String storeId) {
-        switch (storeId) {
-            case "1": return "Steam";
-            case "3": return "GreenManGaming";
-            case "7": return "GOG";
-            case "8": return "EA/Origin";
-            case "11": return "Humble Store";
-            case "13": return "Ubisoft";
-            case "15": return "Fanatical";
-            case "25": return "Epic Games";
-            default: return null;
-        }
     }
 }
